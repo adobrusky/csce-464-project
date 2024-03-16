1164207a1e4fde285fc2f047a71aa35404b1ffe7
adobrusky <austindobrusky@gmail.com>
2024-03-16 12:41:47 -0500

    Add code

---
+++ DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/Helpers/HtmlHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DobruskyCapitalLLC.MVC.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string controller, string action)
+        {
+            RouteData routeData = htmlHelper.ViewContext.RouteData;
+            string routeController = routeData.Values["Controller"].ToString();
+            string routeAction = routeData.Values["Action"].ToString();
+            return controller == routeController && action == routeAction ? "active" : "";
+        }
+
+        public static string HasScrollingNavbarTransition(this IHtmlHelper htmlHelper, string controller, string action)
+        {
+            RouteData routeData = htmlHelper.ViewContext.RouteData;
+            string routeController = routeData.Values["Controller"].ToString();
+            string routeAction = routeData.Values["Action"].ToString();
+            return controller != routeController || action != routeAction ? "scrolled-navbar" : "";
+        }
+    }
+
+}
---
+++ DobruskyCapitalLLC.MVC/DobruskyCapitalLLC.MVC/wwwroot/js/site.js
@@ -0,0 +1,71 @@
+﻿$(document).ready(function () {
+    let lastScrollTop = 0;
+    isHome = !(typeof(isHome) == 'undefined');
+
+    $('body').css('opacity', 1);
+
+    if (!isHome) {
+        $('body').css('padding-top', $('.navbar').outerHeight());
+    }
+
+    function isElementInView($elem) {
+        let docViewTop = $(window).scrollTop();
+        let docViewBottom = docViewTop + $(window).height();
+        let elemTop = $elem.offset().top;
+        return (elemTop <= docViewBottom);
+    }
+
+    function animateElements() {
+        $('.indicator-row').each(function (index) {
+            let $ind = $(this);
+
+            // Calculate a delay based on the element's index to stagger the animations
+            let delay = index * 100;
+
+            if (isElementInView($ind) && !$ind.hasClass('animated')) {
+                setTimeout(function () {
+                    $ind.css({
+                        opacity: '1',
+                        transform: 'translateX(0)'
+                    }).addClass('animated');
+                }, delay);
+            }
+        });
+    }
+
+    function scrollLogic() {
+        if (isHome) {
+            animateElements();
+
+            // Landing page parallax
+            let st = $(this).scrollTop() + 1;
+            if (st > lastScrollTop) {
+                $('.landing-section').css('background-position', `center ${st * -0.15}px`);
+            } else {
+                $('.landing-section').css('background-position', `center ${st * -0.15}px`);
+            }
+            lastScrollTop = st;
+
+            // Adjust home page navbar background color
+            if ($(this).scrollTop() > 0 && !$('#navbar').hasClass('.scrolled-navbar')) {
+                $('#navbar').addClass('scrolled-navbar');
+            } else {
+                $('#navbar').removeClass('scrolled-navbar');
+            }
+        }
+    }
+    scrollLogic();
+    $(window).scroll(function () {
+        scrollLogic();
+    });
+
+    $('#landing-page a').click(function (e) {
+        let aboutPosition = $('#about').offset().top;
+        let navbarHeight = $('.navbar').outerHeight();
+
+        // Scroll to the #about section adjusting for the navbar height
+        $('html, body').stop().animate({
+            scrollTop: aboutPosition - navbarHeight
+        }, 0, 'swing');
+    });
+});
